a7a7d4b568f30c314f42b33b745c01ebad16c69d
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-07-05 01:40:50 -0300

    Adicionando o metodo de login no userController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,17 +1,16 @@
 const Users = require('../model/users')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userController = {
     create: async(req, res) => {
         try {
 
-            const { name, lastName, email } = req.body
-            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            const { name, lastName, email, password } = req.body
+            const hashedPassword = await bcrypt.hash(password, 10)
 
             const emailExist = await Users.findOne({ email });
-            if (emailExist && emailExist._id.toString() !== id) {
-                return res.json({ msg: 'Email já existente' });
-            }
+            if (emailExist) return res.status(400).json({ msg: 'Email ja cadastrado'})
 
             const user = {
                 name,
@@ -88,6 +87,26 @@ const userController = {
             res.status(200).json({updatedUser, msg: 'Usuário autualizado com sucesso'})
         } catch (error) {
             console.log(err)
+        }
+    },
+
+    
+    login: async(req, res) => {
+        try {
+            const { email, password } = req.body
+            const user = await Users.findOne({email})
+
+            if(!user) return res.status(400).json({ msg: 'Usuário não encontrado' })
+
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if(!isMatch) return res.status(401).json({ msg: 'Senha inválida' })
+
+            const token = jwt.sign({ id: user._id }, 'meu_segredo', { expiresIn: '24h' })
+            res.json({ token, user: { id: user._id, name: user.name, email: user.email }})
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ msg: 'Erro ao fazer login. Tente novamente mais tarde.'})
         }
     }
 }
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/usersController')
 const router = express.Router()
 
 router.post('/register', userController.create)
+router.post('/login', userController.login)
 router.get('/show', userController.getAll)
 router.get('/profile/:id', userController.getFindId)
 router.delete('/profile/:id', userController.deleteUser)
