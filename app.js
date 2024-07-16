7387463e540fceda4a0f61bd7378ac475ffa3070
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-07-16 11:55:22 -0300

    Configurando o cors

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const cors = require('cors')
 const app = express()
 
 const corsOptions = {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }
 
-app.options('*', cors(corsOptions)); // Responde a todas as requisições OPTIONS
+app.use(cors(corsOptions)); // Responde a todas as requisições OPTIONS
 app.use(express.json())
 
 
