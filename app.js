f470c4d7eca4f3030de84351c5c8d538c729205f
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-07-16 11:09:59 -0300

    Feito mas ajustes no cors e adicionado o arquivo vercel

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const app = express()
 const corsOptions = {
     origin: 'http://localhost:5173/',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }
 
-app.options('*', cors()); // Responde a todas as requisições OPTIONS
+app.options('*', cors(corsOptions)); // Responde a todas as requisições OPTIONS
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        console.log(res.getHeaders());
+    });
+    next();
+});
+
 
 // DB connection
 database()
